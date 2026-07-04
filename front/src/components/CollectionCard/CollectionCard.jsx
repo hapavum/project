fd328b94cf25f64aca54c0dbe5cb9746828167fb
hapavum <artozmanyan000@gmail.com>
2026-07-04 12:58:19 +0400

    add to file

--- front/src/components/CollectionCard/CollectionCard.jsx
+++ front/src/components/CollectionCard/CollectionCard.jsx
@@ -10,7 +10,7 @@ const CollectionCard = () => {
     <div className={s.container}>
       <div className={s.card} onClick={() => setShowMore(true)}>
         <div className={s.card_img}>
-          <img src={WATCH} alt="Collection" />
+          <img src="https://hips.hearstapps.com/hmg-prod/images/best-titanium-watches-67f507ce533dc.jpg" alt="Collection" />
           <div className={s.overlay}>
             <div className={s.title}>Collection</div>
           </div>
@@ -21,7 +21,7 @@ const CollectionCard = () => {
         <>
           <Link to="/sport" className={`${s.card} ${s.fadeIn}`}>
             <div className={s.card_img}>
-              <img src={WATCH} alt="Sport" />
+              <img src="https://media.wired.com/photos/698f8284d9a9634d320efa36/2:3/w_532,h_798,c_limit/Update-%20Best%20Apple%20Watch_.png" alt="Sport" />
               <div className={s.overlay}>
                 <div className={s.title}>Sport</div>
               </div>
@@ -30,7 +30,7 @@ const CollectionCard = () => {
 
           <Link to="/classic" className={`${s.card} ${s.fadeIn}`}>
             <div className={s.card_img}>
-              <img src={WATCH} alt="Classic" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzefGwKmF41JxODAbsJz5pxWp9p31YNTOE1hjAlkk-JA&s=10" alt="Classic" />
               <div className={s.overlay}>
                 <div className={s.title}>Classic</div>
               </div>
@@ -39,7 +39,7 @@ const CollectionCard = () => {
 
           <Link to="/shop" className={`${s.card} ${s.fadeIn}`}>
             <div className={s.card_img}>
-              <img src={WATCH} alt="Shop" />
+              <img src="https://swcusa.com/cdn/shop/files/Skeleton-10.jpg?v=1761959698&width=2400" alt="Shop" />
               <div className={s.overlay}>
                 <div className={s.title}>Watchs Shop</div>
               </div>
--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import s from "./Footer.module.sass"
 import Container from '../Container/Container'
 import { IoLogoYoutube } from "react-icons/io5"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import { FaGithub } from "react-icons/fa"
 import { BsInstagram } from "react-icons/bs"
 import { FaYandexInternational } from "react-icons/fa6"
@@ -10,22 +10,29 @@ import { FaYandexInternational } from "react-icons/fa6"
 const Footer = () => {
   return (
     <footer className={s.rolex_footer}>
-    <Container>
-      <div className={s.footer_col}>
-         <h4>© 2007—2026  New Watch </h4>
-         <nav className={s.footer_nav}>
-           <Link className={s.link} to="/">Home</Link>
-            <Link className={s.link} to="/sport">New Sport Wartchs</Link>
-            <Link className={s.link} to="/classic">New Classic Watchs</Link>
-         </nav>
-    <IoLogoYoutube />
-    <FaGithub />
-    <BsInstagram />
-    <FaYandexInternational />
+      <Container>
+        <div className={s.footer_col}>
 
-    
-      </div>
-    </Container>
+          <div className={s.brand}>
+            <h4 className={s.logo}>WATCH</h4>
+            <p className={s.copyright}>© 2007—2026 New Watch. All rights reserved.</p>
+          </div>
+
+          <nav className={s.footer_nav}>
+            <Link className={s.link} to="/">Home</Link>
+            <Link className={s.link} to="/sport">New Sport Watches</Link>
+            <Link className={s.link} to="/classic">New Classic Watches</Link>
+          </nav>
+
+          <div className={s.socials}>
+            <a href="#" className={s.iconLink} aria-label="YouTube"><IoLogoYoutube /></a>
+            <a href="#" className={s.iconLink} aria-label="GitHub"><FaGithub /></a>
+            <a href="#" className={s.iconLink} aria-label="Instagram"><BsInstagram /></a>
+            <a href="#" className={s.iconLink} aria-label="Yandex"><FaYandexInternational /></a>
+          </div>
+
+        </div>
+      </Container>
     </footer>
   )
 }
